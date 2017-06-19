5974b96554cc537466a6e075813c090554d23d2f
Qm1ng <qswag1957@gmail.com>
2017-06-19 13:27:50 +0800

    Add files via upload
    filter

--- main.js
+++ main.js
@@ -1,56 +1,46 @@
+//全域變數
+var data; //json parse
+var placeMarker = [];
+var placeInfo = [];
+var contentString = [];
+var map;
+
+
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 22.5956031, lng: 120.306859 },
-        zoom: 14
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 22.5956031,
+            lng: 120.306859
+        },
+        zoom: 13
     });
-    var infoWindow = new google.maps.InfoWindow({ map: map });
+    var infoWindow = new google.maps.InfoWindow({
+        map: map
+    });
 
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('您目前所在位置');
-            map.setCenter(pos);
-        }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-
-
+    //取得json資料
     var xhr = new XMLHttpRequest();
     xhr.open('get', 'https://raw.githubusercontent.com/Qm1ng/html/master/Data.json');
-    xhr.send();
-
-    xhr.onload = function() {
-        const data = JSON.parse(xhr.responseText);
-
-        //建立標記
+    xhr.send(null);
+    xhr.onload = function () {
+        data = JSON.parse(xhr.responseText);
         for (i = 0; i < data.length; i++) {
-            var str = {};
-            var place = {};
-
-            place.lat = parseFloat(data[i]['座標緯度']);
-            place.lng = parseFloat(data[i]['座標經度']);
-
-            str.map = map;
-            str.title = data[i]['創業空間名稱']
-            str.position = place;
-            str.animation = google.maps.Animation.DROP;
-            str.contentString = data[i]['創業空間名稱'];
-            console.log(str);
-            new google.maps.Marker(str);
-
-
+            placeMarker[i] = new google.maps.Marker({
+                map: map,
+                title: data[i]['創業空間名稱'],
+                position: {
+                    lat: parseFloat(data[i]['座標緯度']),
+                    lng: parseFloat(data[i]['座標經度'])
+                },
+                animation: google.maps.Animation.DROP
+                //icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+            });
         }
     }
+
+
+
 
 
 
@@ -60,16 +50,38 @@ function initMap() {
             '發生錯誤 : 取得用戶位置失敗' :
             '發生錯誤 : 您的瀏覽器不支援定位功能');
     }
+    // Try HTML5 geolocation.
+    //取得地區資訊
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('您目前所在位置');
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
 }
 
 
 
+function getRegion($area) {
+    $area = document.getElementById("getRegion").value;
 
-$(document).ready(function() {
+    for (i = 0; i < placeMarker.length; i++) {
+        placeMarker[i].setVisible(false);
 
-    $("#getRegion").change(function() {
-        alert($(this).val());
-    });
-
-
-});
+        if (data[i]['縣市區域'] == $area) {
+            placeMarker[i].setVisible(true);
+            placeMarker[i].setAnimation(google.maps.Animation.DROP);
+        }
+    }
+}
